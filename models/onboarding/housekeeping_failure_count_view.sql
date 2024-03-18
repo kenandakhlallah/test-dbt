687f66a284026f25190f5998df96ae8a2313cb2b
kenandakhlallah <140940613+kenandakhlallah@users.noreply.github.com>
2024-03-19 00:02:29 +0300

    Update housekeeping_failure_count_view.sql

--- models/onboarding/housekeeping_failure_count_view.sql
+++ models/onboarding/housekeeping_failure_count_view.sql
@@ -3,6 +3,6 @@
     materialized="view",
     schema="onboarding"
 ) }}
-with onboarding.housekeeping_failure_count_view as (
+with housekeeping_failure_count_view as (
 SELECT COUNT(*) AS total_failures
 FROM onboarding_housekeeping_failure )
