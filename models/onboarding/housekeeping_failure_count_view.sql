5972fd78bb8314c41b107df0994c5e4346349439
kenandakhlallah <140940613+kenandakhlallah@users.noreply.github.com>
2024-03-19 00:30:25 +0300

    Update housekeeping_failure_count_view.sql

--- models/onboarding/housekeeping_failure_count_view.sql
+++ models/onboarding/housekeeping_failure_count_view.sql
@@ -2,6 +2,16 @@
     materialized="view",
     schema="onboarding"
 ) }}
-with housekeeping_failure_count_view as (
-SELECT COUNT(*) AS total_failures
+with source as (
+    
+    {#-
+    Normally we would select from the table here, but we are using seeds to load
+    our data in this project
+    #}
+    SELECT COUNT(*) AS total_failures
 FROM {{ ref('onboarding_housekeeping_failure') }} )
+
+),
+housekeeping_failure_count_view as (
+SELECT * 
+FROM source )
